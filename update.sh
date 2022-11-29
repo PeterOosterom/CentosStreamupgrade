26e748b1ca2ff65620773e48e9a125fe285020b8
Peter Oosterom <35457893+PeterOosterom@users.noreply.github.com>
2022-11-29 14:21:08 +0100

    Update update.sh

--- update.sh
+++ update.sh
@@ -8,8 +8,6 @@ dnf clean packages
 dnf update -y
 dnf -y groupupdate "Core" "Minimal Install"
 rpm -q kernel-core
-dnf remove -y kernel-core-4.18.0-383.el8.x86_64 kernel-core-4.18.0-408.el8.x86_64
-dnf remove -y inih-56-1.el8
 dnf install -y inih
 rm -f /boot/vmlinuz-0-rescue-b2a198ecbfdd451cb905f76f825af01e /boot/initramfs-0-rescue-b2a198ecbfdd451cb905f76f825af01e.img /boot/loader/entries/b2a198ecbfdd451cb905f76f825af01e-0-rescue.conf
 /usr/lib/kernel/install.d/51-dracut-rescue.install add $(uname -r) /boot /boot/vmlinuz-$(uname -r)
