ef0b8b746355b263e7aa23503f37096a09922cde
Peter Oosterom <35457893+PeterOosterom@users.noreply.github.com>
2022-11-29 20:27:04 +0100

    Update update.sh

--- update.sh
+++ update.sh
@@ -1,6 +1,4 @@
 dnf update -y
-dnf repoquery --extras
-dnf remove -y NetworkManager-initscripts-updown-1:1.40.0-1.el8.noarch grub2-tools-efi-1:2.02-129.el8.x86_64  libmetalink-0:0.1.3-7.el8.x86_64 pciutils-0:3.7.0-1.el8.x86_64 python3-configobj-0:5.0.6-11.el8.noarch python3-schedutils-0:0.6-6.el8.x86_64 rdma-core-0:41.0-1.el8.x86_64
 dnf install -y http://mirror.stream.centos.org/9-stream/BaseOS/x86_64/os/Packages/centos-stream-repos-9.0-18.el9.noarch.rpm http://mirror.stream.centos.org/9-stream/BaseOS/x86_64/os/Packages/centos-stream-release-9.0-18.el9.noarch.rpm http://mirror.stream.centos.org/9-stream/BaseOS/x86_64/os/Packages/centos-gpg-keys-9.0-18.el9.noarch.rpm
 dnf -y --releasever=9-stream --allowerasing --setopt=deltarpm=false distro-sync
 rpmdb --rebuilddb
