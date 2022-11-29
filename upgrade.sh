5db7d404b496a4fb83b50f14bd97f5d3828563cb
Peter Oosterom <35457893+PeterOosterom@users.noreply.github.com>
2022-11-29 13:17:05 +0100

    Update upgrade.sh

--- upgrade.sh
+++ upgrade.sh
@@ -1,19 +1,20 @@
 yum upgrade
+dnf update -y
 dnf install epel-release
 dnf install rpmconf
 dnf install yum-utils
 rpmconf -a  # answer "n" to both things
+dnf repoquery --unneededdnf remove -y NetworkManager-initscripts-updown-1:1.40.0-1.el8.noarch grub2-tools-efi-1:2.02-129.el8.x86_64  libmetalink-0:0.1.3-7.el8.x86_64 pciutils-0:3.7.0-1.el8.x86_64 python3-configobj-0:5.0.6-11.el8.noarch python3-schedutils-0:0.6-6.el8.x86_64 rdma-core-0:41.0-1.el8.x86_64
 package-cleanup --leaves
 package-cleanup --orphans
-dnf install http://mirror.stream.centos.org/9-stream/BaseOS/x86_64/os/Packages/centos-stream-repos-9.0-9.el9.noarch.rpm http://mirror.stream.centos.org/9-stream/BaseOS/x86_64/os/Packages/centos-stream-release-9.0-9.el9.noarch.rpm http://mirror.stream.centos.org/9-stream/BaseOS/x86_64/os/Packages/centos-gpg-keys-9.0-9.el9.noarch.rpm
-curl -O https://dl.fedoraproject.org/pub/epel/epel-release-latest-9.noarch.rpm
-curl -O https://dl.fedoraproject.org/pub/epel/epel-next-release-latest-9.noarch.rpm
+dnf install -y http://mirror.stream.centos.org/9-stream/BaseOS/x86_64/os/Packages/centos-stream-repos-9.0-18.el9.noarch.rpm http://mirror.stream.centos.org/9-stream/BaseOS/x86_64/os/Packages/centos-stream-release-9.0-18.el9.noarch.rpm http://mirror.stream.centos.org/9-stream/BaseOS/x86_64/os/Packages/centos-gpg-keys-9.0-18.el9.noarch.rpm
 rpm -Uvh *.rpm
 yum update dnf module disable python36 python39 perl perl-IO-Socket-SSL perl-libwww-perl
-dnf clean all
 rpm -e `rpm -q kernel`
 dnf -y --releasever=9 --allowerasing --setopt=deltarpm=false distro-sync swap python39-setuptools python3-setuptools
 dnf clean all
 rm -f /var/lib/rpm/__db*
 rpm --rebuilddb
+dnf clean all
 dnf -y groupupdate "Core" "Minimal Install"
+reboot
