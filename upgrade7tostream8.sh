cd8069a9eaf1af414f2a67973d84119943ac9d5e
Peter Oosterom <35457893+PeterOosterom@users.noreply.github.com>
2022-11-30 09:49:47 +0100

    Create upgrade7tostream8.sh

---
+++ upgrade7tostream8.sh
@@ -0,0 +1,16 @@
+yum -y update
+yum -y install epel-release
+yum -y install rpmconf yum-utils
+rpmconf -a
+sudo package-cleanup --leaves
+sudo package-cleanup --orphans
+yum -y install dnf 
+dnf -y update 
+dnf install http://vault.centos.org/8.5.2111/BaseOS/x86_64/os/Packages/{centos-linux-repos-8-3.el8.noarch.rpm,centos-linux-release-8.5-1.2111.el8.noarch.rpm,centos-gpg-keys-8-3.el8.noarch.rpm}
+dnf -y upgrade https://dl.fedoraproject.org/pub/epel/epel-release-latest-8.noarch.rpm
+dnf clean all
+rpm -e `rpm -q kernel`
+rpm -e --nodeps sysvinit-tools
+#now upgrade centos8 to stream 8
+dnf install centos-release-stream
+dnf swap centos-{linux,stream}-repos
