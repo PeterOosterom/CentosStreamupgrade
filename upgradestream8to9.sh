6b8fe3e9db7be84735461295e56acb9c7213fe82
Peter Oosterom <35457893+PeterOosterom@users.noreply.github.com>
2022-12-13 15:16:23 +0100

    Update upgradestream8to9.sh

--- upgradestream8to9.sh
+++ upgradestream8to9.sh
@@ -1,5 +1,5 @@
 dnf update -y
-dnf install -y http://mirror.stream.centos.org/9-stream/BaseOS/x86_64/os/Packages/centos-stream-repos-9.0-18.el9.noarch.rpm http://mirror.stream.centos.org/9-stream/BaseOS/x86_64/os/Packages/centos-stream-release-9.0-18.el9.noarch.rpm http://mirror.stream.centos.org/9-stream/BaseOS/x86_64/os/Packages/centos-gpg-keys-9.0-18.el9.noarch.rpm
+dnf install -y https://download.postgresql.org/pub/repos/yum/reporpms/EL-9-x86_64/pgdg-redhat-repo-latest.noarch.rpm http://mirror.stream.centos.org/9-stream/BaseOS/x86_64/os/Packages/centos-stream-repos-9.0-18.el9.noarch.rpm http://mirror.stream.centos.org/9-stream/BaseOS/x86_64/os/Packages/centos-stream-release-9.0-18.el9.noarch.rpm http://mirror.stream.centos.org/9-stream/BaseOS/x86_64/os/Packages/centos-gpg-keys-9.0-18.el9.noarch.rpm
 dnf -y --releasever=9-stream --allowerasing --setopt=deltarpm=false distro-sync
 rpmdb --rebuilddb
 dnf clean packages
@@ -19,11 +19,17 @@ mv ifcfg-eth0 ifcfg-enX0
 sed -i 's/eth0/enX0/' ifcfg-enX0
 
 dnf list --installed|grep el8
+
 dnf remove -y inih-56-1.el8 iwl6000-firmware
 dnf install -y inih
 
-wget https://dl.rockylinux.org/pub/rocky/9/AppStream/aarch64/os/Packages/m/man-pages-overrides-9.0.0.0-1.el9.noarch.rpm
-yum localinstall man-pages-overrides-9.0.0.0-1.el9.noarch.rpm
+wget https://dl.rockylinux.org/pub/rocky/9/AppStream/x86_64/os/Packages/m/man-pages-overrides-9.0.0.0-1.el9.noarch.rpm 
+wget http://mirror.stream.centos.org/9-stream/BaseOS/x86_64/os/Packages/libfido2-1.6.0-7.el9.x86_64.rpm
+wget http://mirror.stream.centos.org/9-stream/BaseOS/x86_64/os/Packages/libcbor-0.7.0-5.el9.x86_64.rpm
+wget http://mirror.stream.centos.org/9-stream/AppStream/x86_64/os/Packages/javapackages-filesystem-6.0.0-3.el9.noarch.rpm
+wget https://kojihub.stream.centos.org/kojifiles/packages/xorg-x11-font-utils/7.5/53.el9/x86_64/xorg-x11-font-utils-7.5-53.el9.x86_64.rpm
 
+
+yum localinstall -y man-pages-overrides-9.0.0.0-1.el9.noarch.rpm libfido2-1.6.0-7.el9.x86_64.rpm libcbor-0.7.0-5.el9.x86_64.rpm javapackages-filesystem-6.0.0-3.el9.noarch.rpm
 cat /etc/*release
 
